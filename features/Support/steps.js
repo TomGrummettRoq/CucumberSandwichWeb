5d5592d47d055b42c66d82cafad80a98f834d15c
Tom Grummmett <tom.grummett@roq.co.uk>
2023-02-21 16:11:33 +0000

    Additional Tests & Attempt at Text Input

--- features/Support/steps.js
+++ features/Support/steps.js
@@ -3,6 +3,7 @@ const browser = new Browser();
 
 const assert = require('assert');
 const {Given, When, Then, Before, After} = require('@cucumber/cucumber');
+const { time } = require("console");
 
 
 Before(async function() {
@@ -18,12 +19,32 @@ Given('the page loads correctly', function() {
     // Nothing to do as page already loaded
 });
 
-When('the title is clicked', function() {
+// **************************************************//
+//Current Test Work in Progress
+Given('the text input is {word}', async function() {
+    const text = 'ham';
+    const textField = await browser.getElement('ingredient');
+    
+    await textField.sendKeys(text);
+});
+// **************************************************//
 
+When('the button is clicked', async function() {
+    await browser.elementClick('sandwichButton');
 });
 
-Then('the title should change colour', function() {
+When('the heading is clicked', async function() {
+    await browser.elementClick('headingOne');
+});
 
+Then('the screen logs the input', async function() {
+    const text = await browser.getElement('currentFillings');
+    assert(text == 'bread,butter,ham');
+});
+
+Then('the heading should change colour', async function() {
+    const titleColour = await browser.elementColor('headingOne');
+    assert(titleColour == 'rgba(173, 255, 47, 1)');
 });
 
 Then('the title should be {string}', async function(title) {
--- features/index.feature
+++ features/index.feature
@@ -4,15 +4,16 @@ Feature: Website Functionality
         Given the page loads correctly
         Then the title should be "Cucumber Sandwich"
 
-    # Scenario: Clicking the Title changes its colour
-    #     Given the page loads correctly
-    #     When the title is clicked
-    #     Then the title should change colour
+    Scenario: Clicking the Heading One changes its colour
+        Given the page loads correctly
+        When the heading is clicked
+        Then the heading should change colour
 
-    # Scenario: Using the 'add ingredient' form correctly logs input
-    #     Given the text input is "Ham"
-    #     When the button is clicked
-    #     Then the screen logs the input
+    Scenario: Using the 'add ingredient' form correctly logs input
+        Given the page loads correctly
+        And the text input is "ham"
+        When the button is clicked
+        Then the screen logs the input
 
     # Scenario: Clicking the 'Join the Movement' button pops up an alert
     #     Given the page loads correctly
